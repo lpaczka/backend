74d82540e80b7af24ac797e90a831812ee0ed92e
lpaczka <fernando.paczka@gmail.com>
2019-02-14 20:01:26 -0600

    Hola

--- modules/index.js
+++ modules/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const {Post} = require('./post')
 const {User} = require('./users')
 
-// const URL_MONGO = "mongodb://prueba:5ykEfJ48i7fdKhg@ds013956.mlab.com:13956/proyectochingon"
-const URL_MONGO = "mongodb://localhost:27017/proyectofinalprueba"
+const URL_MONGO = "mongodb://lpaczka:Hola12@ds225038.mlab.com:25038/proyectofinal"
+// const URL_MONGO = "mongodb://localhost:27017/proyectofinalprueba"
 mongoose.connect(URL_MONGO, {useNewUrlParser:true}, err => {
     if(!err) console.log('Conexión exitosa')
 })
--- modules/users.js
+++ modules/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-// const URL_MONGO = "mongodb://lpaczka:Hola12@ds225038.mlab.com:25038/proyectofinal"
-const URL_MONGO = "mongodb://localhost:27017/proyectofinalprueba"
+const URL_MONGO = "mongodb://lpaczka:Hola12@ds225038.mlab.com:25038/proyectofinal"
+// const URL_MONGO = "mongodb://localhost:27017/proyectofinalprueba"
 
 mongoose.connect(URL_MONGO, {useNewUrlParser:true}, err => {
     if(!err) console.log('Conexion exitosa')
