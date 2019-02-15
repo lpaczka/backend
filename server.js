17871af0e2345217a9ad4bab0f7693f81f933c6e
lpaczka <fernando.paczka@gmail.com>
2019-02-14 19:48:40 -0600

    message

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send(console.log('Holi'))
+    res.send('Hola')
 })
 
 app.post('/create/user', (req, res) => {
